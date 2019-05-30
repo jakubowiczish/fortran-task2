7c3b74ba19ffbe6d5ea0e114d074e5cc9085aece
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-05-31 01:11:43 +0200

    Update Makefile and main.F90 for testing fftw library, add linking to Makefile

--- src/main.F90
+++ src/main.F90
@@ -1,3 +1,14 @@
 program main
-     write(*,*) "hey"
-end program
+     use, intrinsic :: iso_c_binding 
+     include 'fftw3.f03'
+     type(C_PTR) :: plan
+     complex(C_DOUBLE_COMPLEX), dimension(1024,1000) :: in, out
+     plan = fftw_plan_dft_2d(1000, 1024, in, out, FFTW_FORWARD,FFTW_ESTIMATE)
+     call fftw_execute_dft(plan, in, out)
+   
+     call fftw_destroy_plan(plan)
+
+     write(*,*) "working"
+
+ 
+ end program main
